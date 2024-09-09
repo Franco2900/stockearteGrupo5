11b4a5d3afdb57654f2dd96898776083b49a0375
fer-camacho <camachofernandojl@gmail.com>
2024-09-08 22:38:29 -0300

    creacion db

--- Base de datos/stockearte.sql
+++ Base de datos/stockearte.sql
@@ -1,144 +1,97 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 05-09-2024 a las 17:13:02
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.0.30
+-- MySQL Workbench Forward Engineering
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema stockearte
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema stockearte
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `stockearte` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `stockearte` ;
+
+-- -----------------------------------------------------
+-- Table `stockearte`.`Producto`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `stockearte`.`Producto` (
+  `id` INT NOT NULL,
+  `codigo` VARCHAR(10) NOT NULL,
+  `nombre` VARCHAR(45) NOT NULL,
+  `talle` VARCHAR(45) NOT NULL,
+  `foto` VARCHAR(45) NOT NULL,
+  `color` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+-- -----------------------------------------------------
+-- Table `stockearte`.`Tienda`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `stockearte`.`Tienda` (
+  `id` INT NOT NULL,
+  `codigo` VARCHAR(45) NOT NULL,
+  `direccion` VARCHAR(45) NOT NULL,
+  `ciudad` VARCHAR(45) NOT NULL,
+  `provincia` VARCHAR(45) NOT NULL,
+  `habilitada` TINYINT NOT NULL,
+  `central` TINYINT NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci
+COMMENT = '	';
 
---
--- Base de datos: `stockearte`
---
 
--- --------------------------------------------------------
+-- -----------------------------------------------------
+-- Table `stockearte`.`Tienda_X_Producto`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `stockearte`.`Tienda_X_Producto` (
+  `Producto_id` INT NOT NULL,
+  `Tienda_id` INT NOT NULL,
+  `stock` INT NOT NULL,
+  INDEX `fk_Tienda_has_Producto_Producto1_idx` (`Producto_id` ASC) VISIBLE,
+  INDEX `fk_Tienda_has_Producto_Tienda1_idx` (`Tienda_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Tienda_has_Producto_Producto1`
+    FOREIGN KEY (`Producto_id`)
+    REFERENCES `stockearte`.`Producto` (`id`),
+  CONSTRAINT `fk_Tienda_has_Producto_Tienda1`
+    FOREIGN KEY (`Tienda_id`)
+    REFERENCES `stockearte`.`Tienda` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
---
--- Estructura de tabla para la tabla `productos`
---
 
-CREATE TABLE `productos` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `codigoProducto` varchar(50) NOT NULL,
-  `talle` varchar(50) NOT NULL,
-  `foto` varchar(50) NOT NULL,
-  `color` varchar(50) NOT NULL,
-  `codigoTienda` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- -----------------------------------------------------
+-- Table `stockearte`.`Usuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `stockearte`.`Usuario` (
+  `id` INT NOT NULL,
+  `usuario` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `nombre` VARCHAR(45) NOT NULL,
+  `apellido` VARCHAR(45) NOT NULL,
+  `habilitado` TINYINT NOT NULL,
+  `Tienda_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_Usuario_Tienda_idx` (`Tienda_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Usuario_Tienda`
+    FOREIGN KEY (`Tienda_id`)
+    REFERENCES `stockearte`.`Tienda` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
---
--- Volcado de datos para la tabla `productos`
---
 
-INSERT INTO `productos` (`id`, `nombre`, `codigoProducto`, `talle`, `foto`, `color`, `codigoTienda`) VALUES
-(3, 'Remera', 'YOoKyaALaI', 'L', 'fondo.jpg', 'Verde', '');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tiendas`
---
-
-CREATE TABLE `tiendas` (
-  `id` int(11) NOT NULL,
-  `codigoTienda` varchar(50) NOT NULL,
-  `direccion` varchar(50) NOT NULL,
-  `ciudad` varchar(50) NOT NULL,
-  `provincia` varchar(50) NOT NULL,
-  `habilitado` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tiendasxproductos`
---
-
-CREATE TABLE `tiendasxproductos` (
-  `codigoTienda` varchar(50) NOT NULL,
-  `codigoProducto` varchar(50) NOT NULL,
-  `stock` int(11) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuarios`
---
-
-CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `apellido` varchar(50) NOT NULL,
-  `nombreUsuario` varchar(50) NOT NULL,
-  `contrasenia` varchar(50) NOT NULL,
-  `habilitado` tinyint(1) NOT NULL,
-  `codigoTienda` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `usuarios`
---
-
-INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `nombreUsuario`, `contrasenia`, `habilitado`, `codigoTienda`) VALUES
-(2, 'Pepe', 'Argento', 'El Pepo', '12345', 1, 'sanji32542');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `productos`
---
-ALTER TABLE `productos`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `tiendas`
---
-ALTER TABLE `tiendas`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `productos`
---
-ALTER TABLE `productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT de la tabla `tiendas`
---
-ALTER TABLE `tiendas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
