3ed5806a36e23428759a9f909196cbad3c5f0e46
leanjlg <leanjlg@gmail.com>
2024-10-27 17:15:31 -0300

    Limpeza de codigo Home

--- Front/src/views/Home.jsx
+++ Front/src/views/Home.jsx
@@ -1,13 +1,14 @@
 import Container from "react-bootstrap/Container";
 import React from "react";
-import Logo from "../components/Logo.jsx";
 
 function Home() {
   return (
     <>      
       <section>
         <Container className="h-100 d-flex justify-content-center align-items-center" style={{ minHeight: 'calc(100vh - 112px)' }}>
-          <Logo />
+          <div className="logo-container h-100 d-flex justify-content-center align-items-center">      
+             <h1>STOCKEARTE</h1>
+          </div>
         </Container>
       </section>
     </>
