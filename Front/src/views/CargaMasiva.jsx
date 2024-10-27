86e220f9e270e4b0cd6db91906325ec7f7d4f2de
leanjlg <leanjlg@gmail.com>
2024-10-27 20:38:16 -0300

    ajuste estilos carga masiva

--- Front/src/views/CargaMasiva.jsx
+++ Front/src/views/CargaMasiva.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useContext } from 'react';
 import UserContext from "../context/Context.jsx";
+import { Container, Col, Row} from 'react-bootstrap';
 
 const CargaMasiva = () => {
     const { cargaMasiva } = useContext(UserContext);
@@ -24,14 +25,19 @@ const CargaMasiva = () => {
         }
     };
 
-    return (
-        <div>
-            <h2>Carga Masiva de Usuarios</h2>
-            <form onSubmit={handleSubmit}>
-                <input type="file" onChange={handleFileChange} />
-                <button type="submit">Cargar Archivo</button>
-            </form>
-        </div>
+    return ((<>
+            <Container>
+                <h2>Carga Masiva de Usuarios</h2>
+                <Row className="d-flex justify-content-center align-items-center"style={{overflowY: 'scroll', minHeight: 'calc(100vh -  158px)'}}>
+                  <Col md={{ span: 6 }}>
+                    <form onSubmit={handleSubmit}>
+                        <input type="file" onChange={handleFileChange} />
+                        <button type="submit">Cargar Archivo</button>
+                    </form>
+                  </Col>
+                </Row>
+          </Container>
+          </>)
     );
 };
 
